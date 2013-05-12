568d1c5bb14cf46521f3ed749393a537651b9970
Davis Sorenson <davis@davissorenson.com>
2013-05-12 20:46:05 +0300

    * Loop JACK transport * Stop animation playback when toggling transport to prevent calls to JACK being made when that's not a good idea

--- gui/animationwindow.py
+++ gui/animationwindow.py
@@ -512,6 +512,7 @@ class ToolWidget(gtk.VBox):
         self.ani.framerate = adj.get_value()
 
     def on_jacktransport_toggled(self, checkbox):
+        self.ani.stop_animation()
         self.ani.transport_enabled = checkbox.get_active()
 
         if checkbox.get_active():
